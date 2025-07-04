648a112e1ae1b87718c05849e3aaa6fa8eefd28a
Eduardo Abreu <eduardopradoabreu@gmail.com>
2025-07-03 23:52:05 -0300

    adição buttons de navegação

--- frontend/src/app/components/left-menu/left-menu.component.ts
+++ frontend/src/app/components/left-menu/left-menu.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface BotoesMenuLateral{
+  nome: string,
+  fontAwesomeIcon: string
+}
 
 @Component({
   selector: 'app-left-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './left-menu.component.html',
   styleUrl: './left-menu.component.scss',
 })
-export class LeftMenuComponent {
+export class LeftMenuComponent implements OnInit {
+
+  botoes: BotoesMenuLateral[] = [
+    { nome: 'Home', fontAwesomeIcon: 'fa-solid fa-house' },
+    { nome: 'Serviços', fontAwesomeIcon: 'fa-solid fa-wrench' },
+    { nome: 'Faturamento', fontAwesomeIcon: 'fa-solid fa-file-lines' },
+    { nome: 'Notificações', fontAwesomeIcon: 'fa-solid fa-file-lines' }
+  ]
+
+  ngOnInit(): void {
+    console.log(this.botoes);
+  }
 
 }
